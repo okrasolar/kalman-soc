94ba0c32e91eaca0c10a747a3ebd4b94d8c1c73e
Matthew Johnston <mlarash@mit.edu>
2020-11-16 11:35:30 +0700

    Add csv reading and writing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,124 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility> // std::pair
+#include <stdexcept> // std::runtime_error
+#include <sstream> // std::stringstream
 
-int main(int argc, char **argv) 
-{
-  printf("Hello there.\n");
-  return 0;
+
+void write_csv(std::string filename, std::vector<std::pair<std::string, std::vector<int> > > dataset){
+    // Make a CSV file with one or more columns of integer values
+    // Each column of data is represented by the pair <column name, column data>
+    //   as std::pair<std::string, std::vector<int>>
+    // The dataset is represented as a vector of these columns
+    // Note that all columns should be the same size
+    
+    // Create an output filestream object
+    std::ofstream myFile(filename);
+    
+    // Send column names to the stream
+    for(int j = 0; j < dataset.size(); ++j)
+    {
+        myFile << dataset.at(j).first;
+        if(j != dataset.size() - 1) myFile << ","; // No comma at end of line
+    }
+    myFile << "\n";
+    
+    // Send data to the stream
+    for(int i = 0; i < dataset.at(0).second.size(); ++i)
+    {
+        for(int j = 0; j < dataset.size(); ++j)
+        {
+            myFile << dataset.at(j).second.at(i);
+            if(j != dataset.size() - 1) myFile << ","; // No comma at end of line
+        }
+        myFile << "\n";
+    }
+    
+    // Close the file
+    myFile.close();
+}
+
+std::vector<std::pair<std::string, std::vector<int> > > read_csv(std::string filename){
+    // Reads a CSV file into a vector of <string, vector<int>> pairs where
+    // each pair represents <column name, column values>
+
+    // Create a vector of <string, int vector> pairs to store the result
+    std::vector<std::pair<std::string, std::vector<int> > > result;
+
+    // Create an input filestream
+    std::ifstream myFile(filename);
+
+    // Make sure the file is open
+    if(!myFile.is_open()) throw std::runtime_error("Could not open file");
+
+    // Helper vars
+    std::string line, colname;
+    int val;
+
+    // Read the column names
+    if(myFile.good())
+    {
+        // Extract the first line in the file
+        std::getline(myFile, line);
+
+        // Create a stringstream from line
+        std::stringstream ss(line);
+
+        // Extract each column name
+        while(std::getline(ss, colname, ',')){
+
+            printf("Column: ");
+            printf("%s\n", colname.c_str());
+            
+            // Initialize and add <colname, int vector> pairs to result
+            std::pair<std::string, std::vector<int> > column;
+            result.push_back(column);
+        }
+    }
+
+    // Read data, line by line
+    while(std::getline(myFile, line))
+    {
+        // Create a stringstream of the current line
+        std::stringstream ss(line);
+        
+        // Keep track of the current column index
+        // note: skipping first column because it is a datetime currently -- will need to chnage it to be time difference in milliseconds
+        int colIdx = 0;
+        
+        // Extract each integer
+        while(ss >> val){
+            printf("Val: ");
+            printf("%d\n", val);
+            // Add the current integer to the 'colIdx' column's values vector
+            result.at(colIdx).second.push_back(val);
+            
+            // If the next token is a comma, ignore it and move on
+            if(ss.peek() == ',') ss.ignore();
+            
+            // Increment the column index
+            colIdx++;
+        }
+    }
+
+    // Close file
+    myFile.close();
+
+    return result;
+}
+
+int main() {
+    // Read three_cols.csv and ones.csv
+    std::vector<std::pair<std::string, std::vector<int> > > sensor_data = read_csv("../../data/sensor_data.csv");
+
+    printf("Finished reading.\n");
+
+    // Write to another file to check that this was successful
+    write_csv("../../data/raw_sensor_data.csv", sensor_data);
+
+    printf("Finished writing.\n");
+    
+    return 0;
 }
